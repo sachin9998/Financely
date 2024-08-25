7f7c7a1b2190b008820beca89efbea28364ca808
Sachin Alam <sachinalam4216@gmail.com>
2024-08-25 19:02:04 +0530

    Footer section done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,16 @@
 const Footer = () => {
   return (
-    <footer className="bg-[var(--theme)] text-white p-4 flex items-center justify-center">
+    <footer className="sm:text-base text-sm bg-[var(--theme)] text-white p-3 flex items-center justify-center">
       <p>
         {" "}
-        Created By <span className="font-medium">Sachin Alam</span> | Github |
-        LinkedIn
+        Created By <span className="font-medium">Sachin Alam</span> |{" "}
+        <a href="https://github.com/sachin9998/Financely" target="_blank">
+          Github
+        </a>{" "}
+        |{" "}
+        <a href="https://linkedin.com/in/sachinalam/" target="_blank">
+          Linkedin
+        </a>
       </p>
     </footer>
   );
